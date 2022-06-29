9986929c411afc19f16f6941b81caae5bb01f27e
Gustavo de Paula <33902609+gusdepaula@users.noreply.github.com>
2022-06-29 11:22:07 -0300

    Working with Component Styles

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  // styleUrls: ['./app.component.css'],
+  styles: [
+    `
+      h3 {
+        color: blue;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   name = 'Gustavo';
