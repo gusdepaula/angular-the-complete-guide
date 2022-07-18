5bf970fb1f3d3cd83fef8deb8d2a7a4973fadf67
Gustavo de Paula <33902609+gusdepaula@users.noreply.github.com>
2022-07-18 14:17:53 -0300

    Assiging an Alias to Custom Events

--- my-first-app/src/app/cockpit/cockpit.component.ts
+++ my-first-app/src/app/cockpit/cockpit.component.ts
@@ -10,7 +10,7 @@ export class CockpitComponent implements OnInit {
     serverName: string;
     serverContent: string;
   }>();
-  @Output() blueprintCreated = new EventEmitter<{
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{
     serverName: string;
     serverContent: string;
   }>();
