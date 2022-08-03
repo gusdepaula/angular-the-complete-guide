a5c847b2c752f3fa43830aa833526259a9b634de
Gustavo de Paula <33902609+gusdepaula@users.noreply.github.com>
2022-08-03 12:26:25 -0300

    Passing Ingredients from Recipes to the Shopping List(via Service)

--- ng4-complete-guide/src/app/recipes/recipe.service.ts
+++ ng4-complete-guide/src/app/recipes/recipe.service.ts
@@ -1,7 +1,9 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.module';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.module';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
@@ -20,7 +22,13 @@ export class RecipeService {
     ),
   ];
 
+  constructor(private shoppingListService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
--- ng4-complete-guide/src/app/shopping-list/shopping-list.service.ts
+++ ng4-complete-guide/src/app/shopping-list/shopping-list.service.ts
@@ -16,4 +16,12 @@ export class ShoppingListService {
     this.ingredients.push(ingredient);
     this.ingrendientsChanged.emit(this.ingredients.slice());
   }
+
+  addIngredients(ingredients: Ingredient[]) {
+    // for (let ingredient of ingredients) {
+    //   this.addIngredient(ingredient);
+    // }
+    this.ingredients.push(...ingredients);
+    this.ingrendientsChanged.emit(this.ingredients.slice());
+  }
 }
