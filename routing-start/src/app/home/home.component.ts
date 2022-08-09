f0baab8658992bb69f72cf696cc251c6c7a0f5c2
Gustavo de Paula <33902609+gusdepaula@users.noreply.github.com>
2022-08-09 17:15:50 -0300

    Passing Query Parameters and Fragments

--- routing-start/src/app/home/home.component.ts
+++ routing-start/src/app/home/home.component.ts
@@ -11,8 +11,11 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {}
 
-  onLoadServers() {
+  onLoadServers(id: number) {
     //complex calculation
-    this.router.navigate(["/servers"]);
+    this.router.navigate(["/servers", id, "edit"], {
+      queryParams: { allowEdit: "1" },
+      fragment: "loading",
+    });
   }
 }
