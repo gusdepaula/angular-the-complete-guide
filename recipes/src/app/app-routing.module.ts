3a589221a82822e33ea01dea1a89bde314d0de2d
Gustavo de Paula <33902609+gusdepaula@users.noreply.github.com>
2022-08-16 18:12:53 -0300

    Setting Up Routes

--- recipes/src/app/app-routing.module.ts
+++ recipes/src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/recipes' },
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'recipes', component: RecipesComponent },
   { path: 'shopping-list', component: ShoppingListComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
