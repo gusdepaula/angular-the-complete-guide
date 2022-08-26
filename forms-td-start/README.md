c14eb9c6efe8f117f94f680a8f29bab72f8e0457
Gustavo de Paula <33902609+gusdepaula@users.noreply.github.com>
2022-08-26 17:54:21 -0300

    TD: Creating the Form and Registering the Controls

--- forms-td-start/README.md
+++ forms-td-start/README.md
@@ -1,4 +1,4 @@
-# FormsTdStart
+# Ng4CompleteGuide
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.3.
 
--- forms-td-start/karma.conf.js
+++ forms-td-start/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/forms-td-start'),
+      dir: require('path').join(__dirname, './coverage/ng4-complete-guide'),
       subdir: '.',
       reporters: [
         { type: 'html' },
