7f670440fea8acc5f84269fdc575a679c2dd3d6d
Gustavo de Paula <33902609+gusdepaula@users.noreply.github.com>
2022-09-15 16:05:14 -0300

    Providing the Recipe Service Correctly

--- recipes/src/app/recipes/recipes.component.ts
+++ recipes/src/app/recipes/recipes.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css'],
-  providers: [RecipeService],
 })
 export class RecipesComponent implements OnInit {
-  constructor(private recipeService: RecipeService) {}
+  constructor() {}
 
   ngOnInit() {}
 }
