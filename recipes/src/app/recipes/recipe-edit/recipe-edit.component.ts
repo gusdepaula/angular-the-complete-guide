d530665d8ee2cf21ccdc497cd1ce9294c4ea706c
Gustavo de Paula <33902609+gusdepaula@users.noreply.github.com>
2022-09-15 17:02:20 -0300

    Deleting Ingredients and Some Finishing Touches

--- recipes/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ recipes/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -48,6 +48,10 @@ export class RecipeEditComponent implements OnInit {
     );
   }
 
+  onDeleteIngredient(index: number) {
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
+
   onCancel() {
     this.router.navigate(['../'], { relativeTo: this.route });
   }
