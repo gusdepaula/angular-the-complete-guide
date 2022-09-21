669c4a5d987a43d826ff3a00d70d925902fbec0f
Gustavo de Paula <gdepaula@gmail.com>
2022-09-21 12:47:52 -0300

    Understanding the "async" Pipe

--- pipes-start/src/app/filter.pipe.ts
+++ pipes-start/src/app/filter.pipe.ts
@@ -6,10 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
   transform(value: any, filterString: string, propName: string): any {
-    if (value.length === 0) {
+    if (value.length === 0 || filterString === '') {
       return value;
     }
-
     const resultArray = [];
     for (const item of value) {
       if (item[propName] === filterString) {
