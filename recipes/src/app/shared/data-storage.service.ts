5ee73ba0f5fd67657f1800952032f9484fc349b3
Gustavo de Paula <gdepaula@gmail.com>
2022-09-30 12:28:23 -0300

    Storing Recipes

--- recipes/src/app/shared/data-storage.service.ts
+++ recipes/src/app/shared/data-storage.service.ts
@@ -1,7 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Recipe } from '../recipes/recipe.module';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    return this.http
+      .put(
+        'https://ng-course-recipe-book-d8cfa-default-rtdb.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 }
