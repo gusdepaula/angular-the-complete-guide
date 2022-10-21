4ed58bd4dfdbc3165136130802f5f66f0ae9af8a
Gustavo de Paula <gdepaula@gmail.com>
2022-10-21 14:25:20 -0300

    Getting Started with Reducers

---
+++ recipes/src/app/shopping-list/shopping-list.reducer.ts
@@ -0,0 +1,7 @@
+import { Ingredient } from '../shared/ingredient.module';
+
+const initialState = {
+  ingredients: [new Ingredient('Apples', 5), new Ingredient('Tomates', 10)],
+};
+
+export function shoppingListReducer(state = initialState, action) {}
