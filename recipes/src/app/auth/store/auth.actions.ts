920a203bfeba2b1c2d192b47b97842cd4516e6af
Gustavo de Paula <gdepaula@gmail.com>
2022-10-28 11:56:41 -0300

    Effects & Error Handling

--- recipes/src/app/auth/store/auth.actions.ts
+++ recipes/src/app/auth/store/auth.actions.ts
@@ -21,4 +21,9 @@ export class Logout implements Action {
   readonly type = LOGOUT;
 }
 
-export type AuthActions = Login | Logout;
+export class LoginStart implements Action {
+  readonly type = LOGIN_START;
+
+  constructor(public payload: { email: string; password: string }) {}
+}
+export type AuthActions = Login | Logout | LoginStart;
--- recipes/src/app/auth/store/auth.effects.ts
+++ recipes/src/app/auth/store/auth.effects.ts
@@ -1,8 +1,46 @@
-import { Actions, ofType } from '@ngrx/effects';
+import { HttpClient } from '@angular/common/http';
+import { Actions, ofType, Effect } from '@ngrx/effects';
+import { switchMap, catchError, map } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { environment } from '../../../environments/environment';
 import * as AuthActions from './auth.actions';
 
-export class AuthEffects {
-  authLogin = this.actions$.pipe(ofType(AuthActions.LOGIN_START));
+export interface AuthResponseData {
+  king: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
 
-  constructor(private actions$: Actions) {}
+export class AuthEffects {
+  @Effect()
+  authLogin = this.actions$.pipe(
+    ofType(AuthActions.LOGIN_START),
+    switchMap((authData: AuthActions.LoginStart) => {
+      return this.http
+        .post<AuthResponseData>(
+          'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+            environment.firebaseAPIKey,
+          {
+            email: authData.payload.email,
+            password: authData.payload.password,
+            returnSecureToken: true,
+          }
+        )
+        .pipe(
+          catchError((error) => {
+            //..
+            of();
+          }),
+          map((resData) => {
+            of();
+          })
+        );
+    })
+  );
+
+  constructor(private actions$: Actions, private http: HttpClient) {}
 }
