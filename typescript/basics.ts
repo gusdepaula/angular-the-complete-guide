e2e94aa0f8f8271a1ce69096af8256aef95dac99
Gustavo de Paula <gdepaula@gmail.com>
2022-11-23 11:48:11 -0300

    Array & Objects Types

--- typescript/basics.ts
+++ typescript/basics.ts
@@ -14,3 +14,25 @@ userName = "Gustavo";
 let isInstructor: boolean;
 
 isInstructor = true;
+
+// More complex types
+let hobbies: ["Sports", "Cooking"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Gustavo",
+  age: 39,
+};
+
+// person = {
+//   isEmployee: true,
+// };
+
+let people: {
+  name: string;
+  age: number;
+}[];
