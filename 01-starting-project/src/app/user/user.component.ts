37e0809c734d52a3e4f6a685b885b0fa704f33e0
Gustavo de Paula <gdepaula@gmail.com>
2024-07-18 10:11:58 -0300

    Using Getters For Computed Values

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -13,4 +13,8 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 export class UserComponent {
   selectedUser = DUMMY_USERS[randomIndex];
+
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
 }
