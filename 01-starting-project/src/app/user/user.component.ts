be4dacdf44bafb499cbc48077d6a0fff03a1ecec
Gustavo de Paula <gdepaula@gmail.com>
2024-07-19 15:18:54 -0300

    Managing State & Changing Data

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -19,6 +19,7 @@ export class UserComponent {
   }
 
   onSelectUser() {
-    console.log('Clicked!');
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex];
   }
 }
