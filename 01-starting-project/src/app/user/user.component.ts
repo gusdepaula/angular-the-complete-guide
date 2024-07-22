81b0879baaf7ef56520158b3d6e95e02eeda09dc
Gustavo de Paula <gdepaula@gmail.com>
2024-07-22 11:20:11 -0300

    We Need More Flexible Components!

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,8 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -12,16 +8,5 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
-
-  // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser.avatar;
-  // }
-
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
-  }
+  onSelectUser() {}
 }
