b2672500e186f99c6cdb3abfbf25f4f72079a801
Gustavo de Paula <gdepaula@gmail.com>
2024-08-27 09:58:21 -0300

    Handling Form Submission

--- 02-essentials-practice/src/app/user-input/user-input.component.ts
+++ 02-essentials-practice/src/app/user-input/user-input.component.ts
@@ -1,10 +1,15 @@
 import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: "app-user-input",
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: "./user-input.component.html",
   styleUrl: "./user-input.component.css",
 })
-export class UserInputComponent {}
+export class UserInputComponent {
+  onSubmit() {
+    console.log("Submitted!");
+  }
+}
