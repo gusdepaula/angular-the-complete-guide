486b362d4eef9463205edf11f77bda61f132e61d
Gustavo de Paula <gdepaula@gmail.com>
2024-08-28 09:25:32 -0300

    Calculating the Annual Investment Data

--- 02-essentials-practice/src/app/app.component.ts
+++ 02-essentials-practice/src/app/app.component.ts
@@ -8,4 +8,34 @@ import { UserInputComponent } from "./user-input/user-input.component";
   templateUrl: "./app.component.html",
   imports: [HeaderComponent, UserInputComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  calculateInvestmentResults(data: {
+    initialInvestment: number;
+    duration: number;
+    expectedReturn: number;
+    annualInvestment: number;
+  }) {
+    const { initialInvestment, annualInvestment, expectedReturn, duration } =
+      data;
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    return annualData;
+  }
+}
