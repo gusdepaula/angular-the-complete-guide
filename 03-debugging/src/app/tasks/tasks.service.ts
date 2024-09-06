b3df6739d712cc6d986823cbdefcb53938d5d640
Gustavo de Paula <gdepaula@gmail.com>
2024-09-06 09:14:45 -0300

    Debugging Logical Errors with the Browser DevTools & Breakpoints

--- 03-debugging/src/app/tasks/tasks.service.ts
+++ 03-debugging/src/app/tasks/tasks.service.ts
@@ -1,37 +1,37 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-import { type NewTaskData } from './task/task.model';
+import { type NewTaskData } from "./task/task.model";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class TasksService {
   private tasks = [
     {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
+      id: "t1",
+      userId: "u1",
+      title: "Master Angular",
       summary:
-        'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
+        "Learn all the basic and advanced features of Angular & how to apply them.",
+      dueDate: "2025-12-31",
     },
     {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
+      id: "t2",
+      userId: "u3",
+      title: "Build first prototype",
+      summary: "Build a first prototype of the online shop website",
+      dueDate: "2024-05-31",
     },
     {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
+      id: "t3",
+      userId: "u3",
+      title: "Prepare issue template",
       summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
+        "Prepare and describe an issue template which will help with project management",
+      dueDate: "2024-06-15",
     },
   ];
 
   constructor() {
-    const tasks = localStorage.getItem('tasks');
+    const tasks = localStorage.getItem("tasks");
 
     if (tasks) {
       this.tasks = JSON.parse(tasks);
@@ -45,7 +45,7 @@ export class TasksService {
   addTask(taskData: NewTaskData, userId: string) {
     this.tasks.unshift({
       id: new Date().getTime().toString(),
-      userId: '',
+      userId: userId,
       title: taskData.title,
       summary: taskData.summary,
       dueDate: taskData.date,
@@ -59,6 +59,6 @@ export class TasksService {
   }
 
   private saveTasks() {
-    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    localStorage.setItem("tasks", JSON.stringify(this.tasks));
   }
 }
