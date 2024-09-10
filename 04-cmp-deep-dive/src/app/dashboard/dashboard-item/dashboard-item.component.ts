58cfdc037cf7aa12289b5dedc4e9543b6ef3454c
Gustavo de Paula <gdepaula@gmail.com>
2024-09-10 10:32:08 -0300

    Creating Reusable Components

---
+++ 04-cmp-deep-dive/src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-item.component.html',
+  styleUrl: './dashboard-item.component.css'
+})
+export class DashboardItemComponent {
+
+}
