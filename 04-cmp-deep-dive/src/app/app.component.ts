8c8d5a3988d48aed9c84ead7e3b1273b24112984
Gustavo de Paula <gdepaula@gmail.com>
2024-09-11 10:47:27 -0300

    Property Binding: Repetition

--- 04-cmp-deep-dive/src/app/app.component.ts
+++ 04-cmp-deep-dive/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { HeaderComponent } from "./header/header.component";
 import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
 import { TrafficComponent } from "./dashboard/traffic/traffic.component";
 import { TicketsComponent } from "./dashboard/tickets/tickets.component";
+import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: "app-root",
@@ -12,6 +13,7 @@ import { TicketsComponent } from "./dashboard/tickets/tickets.component";
     ServerStatusComponent,
     TrafficComponent,
     TicketsComponent,
+    DashboardItemComponent,
   ],
   templateUrl: "./app.component.html",
 })
