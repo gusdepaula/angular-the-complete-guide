aa230da1438a9260ea01534bd5aeabc5042b6308
Gustavo de Paula <gdepaula@gmail.com>
2024-09-11 09:57:27 -0300

    Component Inputs: Repetition

--- 04-cmp-deep-dive/src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ 04-cmp-deep-dive/src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from "@angular/core";
 
 @Component({
-  selector: 'app-dashboard-item',
+  selector: "app-dashboard-item",
   standalone: true,
   imports: [],
-  templateUrl: './dashboard-item.component.html',
-  styleUrl: './dashboard-item.component.css'
+  templateUrl: "./dashboard-item.component.html",
+  styleUrl: "./dashboard-item.component.css",
 })
 export class DashboardItemComponent {
+  // @Input({ required: true }) image!: { src: string; alt: string };
+  // @Input({ required: true }) title!: string;
 
+  image = input.required<{ src: string; alt: string }>();
+  title = input.required<string>();
 }
