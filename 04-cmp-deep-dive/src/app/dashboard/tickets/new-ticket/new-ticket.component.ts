7fd2927cbc10b245d53c6516e3df62e069019001
Gustavo de Paula <gdepaula@gmail.com>
2024-09-16 11:22:44 -0300

    Supporting Content Projection with Multiple Slots

--- 04-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 04-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ButtonComponent } from "../../../shared/button/button.component";
 
 @Component({
-  selector: 'app-new-ticket',
+  selector: "app-new-ticket",
   standalone: true,
-  imports: [],
-  templateUrl: './new-ticket.component.html',
-  styleUrl: './new-ticket.component.css'
+  imports: [ButtonComponent],
+  templateUrl: "./new-ticket.component.html",
+  styleUrl: "./new-ticket.component.css",
 })
-export class NewTicketComponent {
-
-}
+export class NewTicketComponent {}
