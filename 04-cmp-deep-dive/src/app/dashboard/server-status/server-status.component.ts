b17d3645792fef2bc2f7bb995bf3739506216e8c
Gustavo de Paula <gdepaula@gmail.com>
2024-09-26 11:10:00 -0300

    Manipulating State & Using Literal Values

--- 04-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ 04-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -8,5 +8,19 @@ import { Component } from "@angular/core";
   styleUrl: "./server-status.component.css",
 })
 export class ServerStatusComponent {
-  currentStatus = "online";
+  currentStatus: "online" | "offline" | "unknown" = "offline";
+
+  constructor() {
+    setInterval(() => {
+      const rnd = Math.random();
+
+      if (rnd < 0.5) {
+        this.currentStatus = "online";
+      } else if (rnd < 0.9) {
+        this.currentStatus = "offline";
+      } else {
+        this.currentStatus = "unknown";
+      }
+    }, 5000);
+  }
 }
