d00f9ec775487d7eb8735c0e69ba7ca78d4f2bb0
Gustavo de Paula <gdepaula@gmail.com>
2024-09-30 17:30:14 -0300

    Component Cleanup with ngOnDestroy

--- 04-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ 04-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-server-status",
@@ -7,14 +7,15 @@ import { Component, OnInit } from "@angular/core";
   templateUrl: "./server-status.component.html",
   styleUrl: "./server-status.component.css",
 })
-export class ServerStatusComponent implements OnInit {
+export class ServerStatusComponent implements OnInit, OnDestroy {
   currentStatus: "online" | "offline" | "unknown" = "offline";
+  private interval?: ReturnType<typeof setInterval>;
 
   constructor() {}
 
   ngOnInit() {
     console.log("ON INIT");
-    setInterval(() => {
+    this.interval = setInterval(() => {
       const rnd = Math.random();
 
       if (rnd < 0.5) {
@@ -29,5 +30,9 @@ export class ServerStatusComponent implements OnInit {
 
   ngAfterViewInit() {
     console.log("AFTER VIEW INIT");
+  }
+
+  ngOnDestroy() {
+    clearTimeout(this.interval);
   }
 }
