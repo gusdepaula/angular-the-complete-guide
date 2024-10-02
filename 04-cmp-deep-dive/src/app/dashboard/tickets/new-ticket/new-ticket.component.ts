a89111857081fb126764e2b13f35ed28633dd196
Gustavo de Paula <gdepaula@gmail.com>
2024-10-02 10:30:24 -0300

    Extracting Input Values via Template Variables

--- 04-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 04-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -11,8 +11,8 @@ import { FormsModule } from "@angular/forms";
   styleUrl: "./new-ticket.component.css",
 })
 export class NewTicketComponent {
-  onSubmit(titleElement: HTMLInputElement) {
-    const enteredTitle = titleElement.value;
-    console.dir("ENTERED TITLE: " + enteredTitle);
+  onSubmit(title: string, ticketText: string) {
+    console.log(title);
+    console.log(ticketText);
   }
 }
