fa46e6bf0b5c8399207441cf8a9b8c1562fa7afc
Gustavo de Paula <gdepaula@gmail.com>
2024-10-11 08:59:28 -0300

    TypeScript & Type Models: Repetition

--- 04-cmp-deep-dive/src/app/dashboard/tickets/tickets.component.ts
+++ 04-cmp-deep-dive/src/app/dashboard/tickets/tickets.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { NewTicketComponent } from "./new-ticket/new-ticket.component";
+import { Ticket } from "./ticket.model";
 
 @Component({
   selector: "app-tickets",
@@ -8,4 +9,6 @@ import { NewTicketComponent } from "./new-ticket/new-ticket.component";
   templateUrl: "./tickets.component.html",
   styleUrl: "./tickets.component.css",
 })
-export class TicketsComponent {}
+export class TicketsComponent {
+  tickets: Ticket[] = [];
+}
