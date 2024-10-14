3fc0cc3ffc3e6c62d4e6ff339d749b1f4da33497
Gustavo de Paula <gdepaula@gmail.com>
2024-10-14 14:40:45 -0300

    Revisiting Inputs & Signals

--- 04-cmp-deep-dive/src/app/dashboard/tickets/ticket/ticket.component.ts
+++ 04-cmp-deep-dive/src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from "@angular/core";
+import { Ticket } from "../ticket.model";
 
 @Component({
-  selector: 'app-ticket',
+  selector: "app-ticket",
   standalone: true,
   imports: [],
-  templateUrl: './ticket.component.html',
-  styleUrl: './ticket.component.css'
+  templateUrl: "./ticket.component.html",
+  styleUrl: "./ticket.component.css",
 })
 export class TicketComponent {
-
+  data = input.required<Ticket>();
 }
