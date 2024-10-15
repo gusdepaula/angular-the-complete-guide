2bb0d82b8b255d290859daaf6418078efad3603d
Gustavo de Paula <gdepaula@gmail.com>
2024-10-15 10:17:03 -0300

    Cross-Component Communication & State Management

--- 04-cmp-deep-dive/src/app/dashboard/tickets/ticket/ticket.component.ts
+++ 04-cmp-deep-dive/src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, signal } from "@angular/core";
+import { Component, input, output, signal } from "@angular/core";
 import { Ticket } from "../ticket.model";
 
 @Component({
@@ -10,10 +10,15 @@ import { Ticket } from "../ticket.model";
 })
 export class TicketComponent {
   data = input.required<Ticket>();
+  close = output();
   detailsVisible = signal(false);
 
   onToggleDetails() {
     // this.detailsVisible.set(!this.detailsVisible());
     this.detailsVisible.update((wasVisible) => !wasVisible);
+  }
+
+  onMarkAsCompleted() {
+    this.close.emit();
   }
 }
--- 04-cmp-deep-dive/src/app/dashboard/tickets/tickets.component.ts
+++ 04-cmp-deep-dive/src/app/dashboard/tickets/tickets.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { NewTicketComponent } from "./new-ticket/new-ticket.component";
 import { Ticket } from "./ticket.model";
 import { TicketComponent } from "./ticket/ticket.component";
+import { tick } from "@angular/core/testing";
 
 @Component({
   selector: "app-tickets",
@@ -22,5 +23,14 @@ export class TicketsComponent {
     };
 
     this.tickets.push(ticket);
+  }
+
+  onCloseTicket(id: string) {
+    this.tickets = this.tickets.map((ticket) => {
+      if (ticket.id === id) {
+        return { ...ticket, status: "close" };
+      }
+      return ticket;
+    });
   }
 }
