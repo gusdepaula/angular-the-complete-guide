281052dca8e947687c2a25c0c67ccc07710e0ecb
Gustavo de Paula <gdepaula@gmail.com>
2024-10-15 10:05:49 -0300

    Updating Signal Values

--- 04-cmp-deep-dive/src/app/dashboard/tickets/ticket/ticket.component.ts
+++ 04-cmp-deep-dive/src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from "@angular/core";
+import { Component, input, signal } from "@angular/core";
 import { Ticket } from "../ticket.model";
 
 @Component({
@@ -10,4 +10,10 @@ import { Ticket } from "../ticket.model";
 })
 export class TicketComponent {
   data = input.required<Ticket>();
+  detailsVisible = signal(false);
+
+  onToggleDetails() {
+    // this.detailsVisible.set(!this.detailsVisible());
+    this.detailsVisible.update((wasVisible) => !wasVisible);
+  }
 }
