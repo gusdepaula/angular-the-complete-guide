186c3ece52fb48747f96b12556d77cca61b47369
Gustavo de Paula <gdepaula@gmail.com>
2024-10-16 10:12:58 -0300

    Two-Way Binding: Repetition

--- 04-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 04-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -24,6 +24,8 @@ export class NewTicketComponent implements OnInit, AfterViewInit {
   @ViewChild("form") form?: ElementRef<HTMLFormElement>;
   //private form = viewChild.required<ElementRef<HTMLFormElement>>("form");
   // @Output() add = new EventEmitter();
+  enteredTitle = "";
+  enteredText = "";
   add = output<{ title: string; text: string }>();
 
   ngOnInit() {
@@ -36,9 +38,11 @@ export class NewTicketComponent implements OnInit, AfterViewInit {
     console.log(this.form?.nativeElement);
   }
 
-  onSubmit(title: string, ticketText: string) {
-    this.add.emit({ title: title, text: ticketText });
+  onSubmit() {
+    this.add.emit({ title: this.enteredTitle, text: this.enteredText });
 
-    this.form?.nativeElement.reset();
+    // this.form?.nativeElement.reset();
+    this.enteredText = "";
+    this.enteredTitle = "";
   }
 }
