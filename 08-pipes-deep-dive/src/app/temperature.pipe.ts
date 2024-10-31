4d77eaccf26f029afa25f6e92430051ac5b36bcf
Gustavo de Paula <gdepaula@gmail.com>
2024-10-31 10:33:46 -0300

    Using Custom Pipes to Perform Custom Transformations

--- 08-pipes-deep-dive/src/app/temperature.pipe.ts
+++ 08-pipes-deep-dive/src/app/temperature.pipe.ts
@@ -5,7 +5,16 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true,
 })
 export class TemperaturePipe implements PipeTransform {
-  transform(value: any, ...args: any[]) {
-    return value + ' - transformed';
+  transform(value: string | number) {
+    let val: number;
+
+    if (typeof value === 'string') {
+      val = parseFloat(value);
+    } else {
+      val = value;
+    }
+
+    const outputTemp = val * (9 / 5) + 32;
+    return `${outputTemp}  ºF`;
   }
 }
