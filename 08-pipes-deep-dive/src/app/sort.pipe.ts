98e5b83a8997152a6dfef14ca2d03467cad91df9
Gustavo de Paula <gdepaula@gmail.com>
2024-11-05 08:44:13 -0300

    Pure & Impure Pipes

--- 08-pipes-deep-dive/src/app/sort.pipe.ts
+++ 08-pipes-deep-dive/src/app/sort.pipe.ts
@@ -3,6 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'sort',
   standalone: true,
+  pure: false
 })
 export class SortPipe implements PipeTransform {
   transform(value: string[] | number[], direction: 'asc' | 'desc' = 'asc') {
