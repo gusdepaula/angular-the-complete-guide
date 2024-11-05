f4b1cbbb9f60af739eca07cb3394d38463a0969e
Gustavo de Paula <gdepaula@gmail.com>
2024-11-05 08:42:38 -0300

    Understanding How Pipes Are Executed

--- 08-pipes-deep-dive/src/app/app.component.ts
+++ 08-pipes-deep-dive/src/app/app.component.ts
@@ -23,6 +23,9 @@ export class AppComponent {
   ];
 
   onReset(index: number) {
-    this.historicTemperatures[index] = 18;
+    // this.historicTemperatures[index] = 18;
+    const newTemps = [...this.historicTemperatures]
+    newTemps[index] = 18;
+    this.historicTemperatures = newTemps;
   }
 }
