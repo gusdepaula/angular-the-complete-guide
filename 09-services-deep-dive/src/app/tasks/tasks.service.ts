6a72a17e2f475f0fe1889c7749c577b7472d8965
Gustavo de Paula <gdepaula@gmail.com>
2024-11-18 10:45:03 -0300

    Understanding the Element Injector's Behavior

--- 09-services-deep-dive/src/app/tasks/tasks.service.ts
+++ 09-services-deep-dive/src/app/tasks/tasks.service.ts
@@ -1,9 +1,9 @@
 import { Injectable, signal } from "@angular/core";
 import { Task, TaskStatus } from "./task.model";
 
-// @Injectable({
-//     providedIn: 'root'
-// })
+@Injectable({
+    providedIn: 'root'
+})
 
 export class TaskService {
     private tasks = signal<Task[]>([]);
