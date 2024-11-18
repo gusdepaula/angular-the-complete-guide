49e1b3dd990453eaacdedbf4469a13593aed261e
Gustavo de Paula <gdepaula@gmail.com>
2024-11-18 10:41:02 -0300

    Providing Services via the Element Injector

--- 09-services-deep-dive/src/main.ts
+++ 09-services-deep-dive/src/main.ts
@@ -3,6 +3,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { TaskService } from './app/tasks/tasks.service';
 
-bootstrapApplication(AppComponent, {
-    providers: [TaskService]
-}).catch((err) => console.error(err));
+// bootstrapApplication(AppComponent, {
+//     providers: [TaskService]
+// }).catch((err) => console.error(err));
+
+bootstrapApplication(AppComponent).catch((err) => console.error(err));
