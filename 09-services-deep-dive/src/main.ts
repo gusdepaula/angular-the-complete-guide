69d22cd4bd536dd92e3295768d0712e3647d34b9
Gustavo de Paula <gdepaula@gmail.com>
2024-11-19 10:02:52 -0300

    Analyzing Dependency Injection with the Angular DevTools

--- 09-services-deep-dive/src/main.ts
+++ 09-services-deep-dive/src/main.ts
@@ -2,9 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 import { TaskService } from './app/tasks/tasks.service';
+import { LoggingService } from './app/logging.service';
 
 // bootstrapApplication(AppComponent, {
 //     providers: [TaskService]
 // }).catch((err) => console.error(err));
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent,{providers: [LoggingService]}).catch((err) => console.error(err));
