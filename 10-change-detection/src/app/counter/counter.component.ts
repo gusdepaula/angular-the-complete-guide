fabffbae5f6a76e1a0afb00cbd224455744f3809
Gustavo de Paula <gdepaula@gmail.com>
2024-12-19 14:07:17 -0300

    Avoiding Zone Pollution

--- 10-change-detection/src/app/counter/counter.component.ts
+++ 10-change-detection/src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, NgZone, OnInit, signal } from '@angular/core';
 
 import { InfoMessageComponent } from '../info-message/info-message.component';
 
@@ -9,7 +9,8 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   styleUrl: './counter.component.css',
   imports: [InfoMessageComponent],
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
+  private zone = inject(NgZone);
   count = signal(0);
 
   get debugOutput() {
@@ -17,11 +18,23 @@ export class CounterComponent {
     return 'Counter Component Debug Output';
   }
 
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.count.set(0);
+    }, 4000);
+
+    this.zone.runOutsideAngular(() => {
+      setTimeout(() => {
+        console.log('Times expired');
+      }, 5000);
+    });
+  }
+
   onDecrement() {
-    this.count.update((prevCount) => prevCount - 1);
+    this.count.update(prevCount => prevCount - 1);
   }
 
   onIncrement() {
-    this.count.update((prevCount) => prevCount + 1);
+    this.count.update(prevCount => prevCount + 1);
   }
 }
