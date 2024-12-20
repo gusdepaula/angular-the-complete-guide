ff6bd0e1bddb324495d77d86edaded298ef507e7
Gustavo de Paula <gdepaula@gmail.com>
2024-12-20 11:11:57 -0300

    Understanding the OnPush Strategy

--- 10-change-detection/src/app/counter/counter.component.ts
+++ 10-change-detection/src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, NgZone, OnInit, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, NgZone, OnInit, signal } from '@angular/core';
 
 import { InfoMessageComponent } from '../info-message/info-message.component';
 
@@ -8,6 +8,7 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css',
   imports: [InfoMessageComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CounterComponent implements OnInit {
   private zone = inject(NgZone);
--- 10-change-detection/src/app/messages/new-message/new-message.component.ts
+++ 10-change-detection/src/app/messages/new-message/new-message.component.ts
@@ -1,4 +1,4 @@
-import { Component, output, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -7,6 +7,7 @@ import { FormsModule } from '@angular/forms';
   imports: [FormsModule],
   templateUrl: './new-message.component.html',
   styleUrl: './new-message.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NewMessageComponent {
   add = output<string>();
