dd12f03218de86216455f205c6e0c802f872b99f
Gustavo de Paula <gdepaula@gmail.com>
2025-03-27 13:35:58 -0300

    Implement signup form with validation and reset functionality

--- 13-forms/src/app/auth/signup/signup.component.ts
+++ 13-forms/src/app/auth/signup/signup.component.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, DestroyRef, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  private destroyRef = inject(DestroyRef);
+
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.email, Validators.required],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+  });
+
+  get emailIsInvalid() {
+    return (
+      this.form.controls.email.invalid &&
+      this.form.controls.email.touched &&
+      this.form.controls.email.dirty
+    );
+  }
+
+  get passwordIsInvalid() {
+    return (
+      this.form.controls.password.invalid &&
+      this.form.controls.password.touched &&
+      this.form.controls.password.dirty
+    );
+  }
+
+  onSubmit() {}
+
+  onReset() {
+    this.form.reset();
+  }
+}
