8ecd8d7f80f24ef1cf003ce042fc50bb90bdd3c5
Gustavo de Paula <gdepaula@gmail.com>
2025-03-31 14:57:45 -0300

    Setting Up & Navigating To Dynamic Routes

--- 14-routing/src/app/app.routes.ts
+++ 14-routing/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { TaskComponent } from './tasks/task/task.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
+import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 
 export const routes: Routes = [
   {
@@ -8,7 +9,7 @@ export const routes: Routes = [
     component: NoTaskComponent,
   },
   {
-    path: 'tasks',
-    component: TaskComponent,
+    path: 'users/:userId',
+    component: UserTasksComponent,
   },
 ];
