1a7a40ed183ecbdd19b5ab6a2175a36d3f98e570
Gustavo de Paula <gdepaula@gmail.com>
2025-03-31 14:34:22 -0300

    Enabling Routing & Adding a First Route

---
+++ 14-routing/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { TaskComponent } from './tasks/task/task.component';
+
+export const routes: Routes = [
+  {
+    path: 'tasks',
+    component: TaskComponent,
+  },
+];
--- 14-routing/src/main.ts
+++ 14-routing/src/main.ts
@@ -1,5 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
