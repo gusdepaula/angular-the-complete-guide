8f9604c82e6afb53c6c14311b07acc622e5f3f2e
Gustavo de Paula <gdepaula@gmail.com>
2025-04-01 10:01:05 -0300

    Redirecting Users

--- 14-routing/src/app/app.routes.ts
+++ 14-routing/src/app/app.routes.ts
@@ -1,39 +1,21 @@
 import { Routes } from '@angular/router';
-
-import { TasksComponent } from './tasks/tasks.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 export const routes: Routes = [
   {
-    path: '', // <your-domain>/
+    path: '',
     component: NoTaskComponent,
-    // redirectTo: '/users/u1',
-    // pathMatch: 'full'
   },
   {
-    path: 'users/:userId', // <your-domain>/users/<uid>
+    path: 'users/:userId',
     component: UserTasksComponent,
     children: [
-      {
-        path: '',
-        redirectTo: 'tasks',
-        pathMatch: 'full',
-      },
-      {
-        path: 'tasks', // <your-domain>/users/<uid>/tasks
-        component: TasksComponent,
-      },
-      {
-        path: 'tasks/new',
-        component: NewTaskComponent,
-      },
+      { path: '', redirectTo: 'tasks', pathMatch: 'full' },
+      { path: 'tasks', component: TasksComponent },
+      { path: 'tasks/new', component: NewTaskComponent },
     ],
-  },
-  {
-    path: '**',
-    component: NotFoundComponent,
   },
 ];
