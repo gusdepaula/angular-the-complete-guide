0acdcb06f173e497c03e4a2ffd5cea5589f60ab3
Gustavo de Paula <gdepaula@gmail.com>
2025-04-01 10:05:24 -0300

    Splitting Route Definitions Across Multiple Files

--- 14-routing/src/app/app.routes.ts
+++ 14-routing/src/app/app.routes.ts
@@ -1,8 +1,7 @@
 import { Routes } from '@angular/router';
+import { routes as userRoutes } from './users/users.routes';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
-import { TasksComponent } from './tasks/tasks.component';
 
 export const routes: Routes = [
   {
@@ -12,10 +11,6 @@ export const routes: Routes = [
   {
     path: 'users/:userId',
     component: UserTasksComponent,
-    children: [
-      { path: '', redirectTo: 'tasks', pathMatch: 'full' },
-      { path: 'tasks', component: TasksComponent },
-      { path: 'tasks/new', component: NewTaskComponent },
-    ],
+    children: userRoutes,
   },
 ];
---
+++ 14-routing/src/app/users/users.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { TasksComponent } from '../tasks/tasks.component';
+import { NewTaskComponent } from '../tasks/new-task/new-task.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'tasks', pathMatch: 'full' },
+  {
+    path: 'tasks', // <your-domain>/users/<userId>/tasks
+    component: TasksComponent,
+  },
+  { path: 'tasks/new', component: NewTaskComponent },
+];
