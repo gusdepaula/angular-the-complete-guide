dc21d5bbc502222c2e40987106bafd67bbb8fd83
Gustavo de Paula <gdepaula@gmail.com>
2025-04-01 09:53:51 -0300

    Adding A "Not Found" Route

--- 14-routing/src/app/app.routes.ts
+++ 14-routing/src/app/app.routes.ts
@@ -1,20 +1,39 @@
 import { Routes } from '@angular/router';
-import { TaskComponent } from './tasks/task/task.component';
+
+import { TasksComponent } from './tasks/tasks.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: '', // <your-domain>/
     component: NoTaskComponent,
+    // redirectTo: '/users/u1',
+    // pathMatch: 'full'
   },
   {
-    path: 'users/:userId',
+    path: 'users/:userId', // <your-domain>/users/<uid>
     component: UserTasksComponent,
     children: [
-      { path: 'tasks', component: TaskComponent },
-      { path: 'tasks/new', component: NewTaskComponent },
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full',
+      },
+      {
+        path: 'tasks', // <your-domain>/users/<uid>/tasks
+        component: TasksComponent,
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+      },
     ],
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
   },
 ];
