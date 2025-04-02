704607aa3f1937cddf8717438aa5d2221dd2ecc9
Gustavo de Paula <gdepaula@gmail.com>
2025-04-02 10:36:36 -0300

    Controlling Route Resolver Execution

--- 14-routing/src/app/tasks/tasks.component.ts
+++ 14-routing/src/app/tasks/tasks.component.ts
@@ -1,18 +1,9 @@
-import {
-  Component,
-  computed,
-  DestroyRef,
-  inject,
-  input,
-  OnInit,
-  signal,
-  Signal,
-} from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { ResolveFn, RouterLink } from '@angular/router';
 
 import { TaskComponent } from './task/task.component';
-import { Task } from './task/task.model';
 import { TasksService } from './tasks.service';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Task } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
@@ -21,33 +12,29 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
   styleUrl: './tasks.component.css',
   imports: [TaskComponent, RouterLink],
 })
-export class TasksComponent implements OnInit {
+export class TasksComponent {
+  userTasks = input.required<Task[]>();
   userId = input.required<string>();
-  // order = input<'desc' | 'asc'>();
-  order = signal<'desc' | 'asc'>('desc');
-  private tasksService = inject(TasksService);
-  private activatedRoute = inject(ActivatedRoute);
-  private destroyRef = inject(DestroyRef);
-
-  userTasks = computed(() =>
-    this.tasksService
-      .allTasks()
-      .filter((task) => task.userId === this.userId())
-      .sort((a, b) => {
-        if (this.order() === 'desc') {
-          return a.id > b.id ? -1 : 1;
-        } else {
-          return a.id > b.id ? 1 : -1;
-        }
-      })
-  );
-
-  ngOnInit(): void {
-    const subscription = this.activatedRoute.queryParams.subscribe({
-      next: (params) => {
-        this.order.set(params['order']);
-      },
-    });
-    this.destroyRef.onDestroy(() => subscription.unsubscribe());
-  }
+  order = input<'asc' | 'desc' | undefined>();
 }
+
+export const resolveUserTasks: ResolveFn<Task[]> = (
+  activatedRouteSnapshot,
+  routerState
+) => {
+  const order = activatedRouteSnapshot.queryParams['order'];
+  const tasksService = inject(TasksService);
+  const tasks = tasksService
+    .allTasks()
+    .filter(
+      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
+    );
+
+  if (order && order === 'asc') {
+    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
+  } else {
+    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
+  }
+
+  return tasks.length ? tasks : [];
+};
--- 14-routing/src/app/users/users.routes.ts
+++ 14-routing/src/app/users/users.routes.ts
@@ -1,12 +1,24 @@
 import { Routes } from '@angular/router';
-import { TasksComponent } from '../tasks/tasks.component';
+
+import { TasksComponent, resolveUserTasks } from '../tasks/tasks.component';
 import { NewTaskComponent } from '../tasks/new-task/new-task.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'tasks', pathMatch: 'full' },
   {
-    path: 'tasks', // <your-domain>/users/<userId>/tasks
-    component: TasksComponent,
+    path: '',
+    redirectTo: 'tasks',
+    pathMatch: 'full',
   },
-  { path: 'tasks/new', component: NewTaskComponent },
+  {
+    path: 'tasks', // <your-domain>/users/<uid>/tasks
+    component: TasksComponent,
+    runGuardsAndResolvers: 'paramsOrQueryParamsChange',
+    resolve: {
+      userTasks: resolveUserTasks,
+    },
+  },
+  {
+    path: 'tasks/new',
+    component: NewTaskComponent,
+  },
 ];
