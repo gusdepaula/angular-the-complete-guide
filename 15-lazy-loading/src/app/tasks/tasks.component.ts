fb4cd4b079950f6bdf9eaef52de024e1ab2743cb
Gustavo de Paula <gdepaula@gmail.com>
2025-04-03 10:33:53 -0300

    Implementing Route-based Lazy Loading

--- 15-lazy-loading/src/app/tasks/tasks.component.ts
+++ 15-lazy-loading/src/app/tasks/tasks.component.ts
@@ -1,12 +1,6 @@
-import {
-  Component,
-  inject,
-  input,
-} from '@angular/core';
-import { ResolveFn, RouterLink } from '@angular/router';
-
+import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TaskComponent } from './task/task.component';
-import { TasksService } from './tasks.service';
 import { Task } from './task/task.model';
 
 @Component({
@@ -21,24 +15,3 @@ export class TasksComponent {
   userId = input.required<string>();
   order = input<'asc' | 'desc' | undefined>();
 }
-
-export const resolveUserTasks: ResolveFn<Task[]> = (
-  activatedRouteSnapshot,
-  routerState
-) => {
-  const order = activatedRouteSnapshot.queryParams['order'];
-  const tasksService = inject(TasksService);
-  const tasks = tasksService
-    .allTasks()
-    .filter(
-      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
-    );
-
-  if (order && order === 'asc') {
-    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
-  } else {
-    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
-  }
-
-  return tasks.length ? tasks : [];
-};
--- 15-lazy-loading/src/app/users/users.routes.ts
+++ 15-lazy-loading/src/app/users/users.routes.ts
@@ -1,7 +1,32 @@
-import { Routes } from '@angular/router';
+import { ResolveFn, Routes } from '@angular/router';
+import {
+  NewTaskComponent,
+  canLeaveEditPage,
+} from '../tasks/new-task/new-task.component';
+import { TasksService } from '../tasks/tasks.service';
+import { inject } from '@angular/core';
+import { Task } from '../tasks/task/task.model';
 
-import { TasksComponent, resolveUserTasks } from '../tasks/tasks.component';
-import { NewTaskComponent, canLeaveEditPage } from '../tasks/new-task/new-task.component';
+const resolveUserTasks: ResolveFn<Task[]> = (
+  activatedRouteSnapshot,
+  routerState
+) => {
+  const order = activatedRouteSnapshot.queryParams['order'];
+  const tasksService = inject(TasksService);
+  const tasks = tasksService
+    .allTasks()
+    .filter(
+      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
+    );
+
+  if (order && order === 'asc') {
+    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
+  } else {
+    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
+  }
+
+  return tasks.length ? tasks : [];
+};
 
 export const routes: Routes = [
   {
@@ -11,7 +36,10 @@ export const routes: Routes = [
   },
   {
     path: 'tasks', // <your-domain>/users/<uid>/tasks
-    component: TasksComponent,
+    loadComponent: () =>
+      import('../tasks/tasks.component').then(
+        (module) => module.TasksComponent
+      ),
     runGuardsAndResolvers: 'always',
     resolve: {
       userTasks: resolveUserTasks,
@@ -20,6 +48,6 @@ export const routes: Routes = [
   {
     path: 'tasks/new',
     component: NewTaskComponent,
-    canDeactivate: [canLeaveEditPage]
+    canDeactivate: [canLeaveEditPage],
   },
 ];
